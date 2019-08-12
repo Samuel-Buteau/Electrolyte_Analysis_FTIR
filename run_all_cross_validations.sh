fb89c56194aee90db4619cd8c9be254e35c8ec16
Samuel Buteau <Samuel.Buteau@dal.ca>
2019-08-12 09:36:09 -0300

    better plots for small journal.

--- run_all_cross_validations.sh
+++ run_all_cross_validations.sh
@@ -1,2 +1,4 @@
-sh run_cross_validations.sh
-sh run_cross_validations_constant.sh
+sh run_cross_validations.sh &
+sh run_cross_validations_constant.sh &
+sh run_cross_validations.sh &
+sh run_cross_validations_constant.sh &
